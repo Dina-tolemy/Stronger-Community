706c974e39ccd396a1be44a74daf4345b6d73d27
Dinatolemy <dina.a.tolemy@gmail.com>
2020-05-06 14:53:11 +1000

    add tooltip to the delete button

--- client/src/components/DeleteButton/deletebutton.js
+++ client/src/components/DeleteButton/deletebutton.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./style.css";
+import Tippy from "@tippy.js/react";
+import "tippy.js/dist/tippy.css";
 
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
 function DeleteBtn(props) {
   return (
-    <span className="delete-btn"   aria-label="Delete User" {...props} role="button">
-      ✗
-    </span>
+    <div>
+      <Tippy className="allpeopleTooltip" content="Delete this service">
+        <span className="delete-btn" {...props} role="button">
+          ✗
+        </span>
+      </Tippy>
+    </div>
   );
 }
 
